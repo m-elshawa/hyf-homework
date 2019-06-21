d8b5c5207cb3d8797cd97f4bebc9e8db9300489a
m-elshawa <49238049+m-elshawa@users.noreply.github.com>
2019-06-21 13:59:38 +0200

    Dog-age

---
+++ javascript/javascript1/week1/dog-age.js
@@ -0,0 +1,15 @@
+console.log('dog-age')
+
+const dogYearOfBirth = 2015;
+var dogYearFuture = 2027;
+let dogYear = dogYearFuture - dogYearOfBirth;
+
+let shouldShowResultsInDogYears = true
+
+if (shouldShowResultsInDogYears) {let  result = dogYear * 2;
+
+    console.log("Your dog will be " + dogYear + "dog years old in " + dogYearFuture)
+} else {console.log("Your dog will be " + result + "dog years old in " + dogYearFuture)
+    
+}
+
