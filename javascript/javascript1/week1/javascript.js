266789589b519d36b3c4d4b4ec378a35c90e5688
m-elshawa <49238049+m-elshawa@users.noreply.github.com>
2019-06-21 13:34:36 +0200

    age-ify

--- javascript/javascript1/week1/javascript.js
+++ javascript/javascript1/week1/javascript.js
@@ -1,15 +1,11 @@
-console.log('dog-age')
+const yearOfBirth = 1984;
+let yearFuture = 2020;
 
-const dogYearOfBirth = 2015;
-var dogYearFuture = 2027;
-let dogYear = dogYearFuture - dogYearOfBirth;
+let age = yearFuture - yearOfBirth;
 
-let shouldShowResultsInDogYears = true
 
-if (shouldShowResultsInDogYears) {let  result = dogYear * 2;
+console.log("You will be " + age + "years old in " + yearFuture);
 
-    console.log("Your dog will be " + dogYear + "dog years old in " + dogYearFuture)
-} else {console.log("Your dog will be " + result + "dog years old in " + dogYearFuture)
-    
-}
+
+ 
 
