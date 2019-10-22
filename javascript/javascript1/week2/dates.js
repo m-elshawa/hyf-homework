215fb3ad15e1cc7ec1c6be76724ab4fe23417073
m-elshawa <49238049+m-elshawa@users.noreply.github.com>
2019-10-22 12:27:47 +0200

    Add files via upload

---
+++ javascript/javascript1/week2/dates.js
@@ -0,0 +1,11 @@
+var nowDate = ('2019, 09, 20');
+
+function futureDate(2019, 09, 24) {
+   var daysLeft = (futureDate - nowDate);
+   return daysLeft;
+
+}
+
+console.log(daysLeft + 'Days' + 'Left');
+
+// not sure of the right answer
