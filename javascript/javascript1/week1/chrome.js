4bc3b84377a0b474ceff53bed10b9570d0ef6add
m-elshawa <49238049+m-elshawa@users.noreply.github.com>
2020-03-03 16:22:43 +0100

    Add files via upload
    followup excercies

---
+++ javascript/javascript1/week1/chrome.js
@@ -0,0 +1,30 @@
+console.log (typeof 3);
+console.log(typeof -33);
+console.log(typeof '3');
+
+const threeConst = 3;
+console.log(threeConst);
+
+let threeLet = 3;
+console.log(threeLet);
+
+console.log(typeof 'console.log("console.log(console.log(""))")');
+
+console.log(typeof typeof 45);
+
+const names = ['benjamin', 'Christopher'];
+console.log(typeof names[0]);
+
+console.log(typeof [3][0]);
+
+console.log(typeof true);
+
+var x = (24*55);
+console.log(x);
+
+const myName = 'Mohamed';
+console.log(myName[0]);
+console.log(myName[6]);
+
+var myArr = ['Mizo', 'Mino', 'Mido', 4, 8, 12, true, false, false];
+console.log(myArr[4]);
