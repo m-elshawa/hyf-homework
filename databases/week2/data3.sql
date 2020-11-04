99eef8a0ad98c59f2a455e42495887de006ac359
m-elshawa <mmelshawa@gmail.com>
2020-11-04 21:09:57 +0100

    implemented

--- databases/week2/data3.sql
+++ databases/week2/data3.sql
@@ -13,14 +13,14 @@ SELECT user.name AS user_name, task.title AS the_task, task.status_id, status.na
 FROM user
 JOIN task          -- when I put condition ON user.id = task.user_id it doesn't give me rows in the table
 JOIN status ON status.id = task.status_id
-WHERE user.name LIKE 'donald duck' 
+WHERE user.name LIKE '%donald duck%' 
 AND status_id = 1;
 
 --Maryrose Meadows
 SELECT user.name, task.title, task.created
 FROM user
 LEFT JOIN task ON user.id = task.user_id
-WHERE user.name LIKE 'Maryrose Meadows'
+WHERE user.name LIKE '%Maryrose Meadows%'
 AND month(created)= 9;
 
 --Tasks created each month
