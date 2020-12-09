dafdf5822ff998fca996d0b51261ec9283ccacce
m-elshawa <mmelshawa@gmail.com>
2020-12-09 20:01:53 +0100

    Implemented week1 Homework

--- react/week1/todo-list-app/src/App.js
+++ react/week1/todo-list-app/src/App.js
@@ -1,21 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Todolist from './Components/Todolist';
-import Getup from './Components/Getup'
-import Brush from './Components/Brush';
-import Eat from './Components/Eat';
+import TodoList from './Components/TodoList';
 import Useclasses from './Components/Useclasses'
+
 
 function App() {
   return (
     <div className="App">
-      <Todolist />
-      {/* <Getup description = "Get out of bed" deadline = "Wed Sep 13 2017" /> */}
-      {/* <Brush description = "Brush my teeth" deadline = "Thu Sep 14 2017" /> */}
-      {/* <Eat description = "Eat breakfast" deadline = "Fri Sep 15 2017" /> */}
-      <Useclasses description = "Get out of bed" deadline = "Wed Sep 13 2017" />
+      <h1>Todo List</h1>
+      <TodoList />
+      
+      
+      {/* <Useclasses description = "Get out of bed" deadline = "Wed Sep 13 2017" />
       <Useclasses description = "Brush my teeth" deadline = "Thu Sep 14 2017" />
-      <Useclasses description = "Eat breakfast" deadline = "Fri Sep 15 2017" />
+      <Useclasses description = "Eat breakfast" deadline = "Fri Sep 15 2017" /> */}
+      
     </div>
   );
 }
